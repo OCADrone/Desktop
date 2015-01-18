debd68aca4e292c2357f3a93fefa5cfb5fd60bc5
Max Desplanches <max.desplanches@gmail.com>
2015-01-18 19:47:32 +0100

    [OCADrone Tools] - Action on click Button updated.

--- OCADrone_Tools/mainwindow.cpp
+++ OCADrone_Tools/mainwindow.cpp
@@ -13,3 +13,29 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_ocadronedeskrop_clicked()
+{
+    QString program = "OCADrone_Desktop.exe";
+    QProcess *myProcess = new QProcess(this);
+    myProcess->start(program, 0);
+}
+
+void MainWindow::on_createmodule_clicked()
+{
+    QString program = "Create_Module.exe";
+    QProcess *myProcess = new QProcess(this);
+    myProcess->start(program, 0);
+}
+
+void MainWindow::on_xmlcreator_clicked()
+{
+    QString program = "XMLCreator.exe";
+    QProcess *myProcess = new QProcess(this);
+    myProcess->start(program, 0);
+}
+
+void MainWindow::on_website_clicked()
+{
+    QDesktopServices::openUrl(QUrl("http://www.ocadrone.com"));
+}
--- OCADrone_Tools/mainwindow.h
+++ OCADrone_Tools/mainwindow.h
@@ -2,21 +2,31 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QProcess>
+#include <QDesktopServices>
+#include <QUrl>
 
-namespace Ui {
+namespace Ui
+{
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
+class MainWindow :      public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit            MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void                on_ocadronedeskrop_clicked();
+    void                on_createmodule_clicked();
+    void                on_xmlcreator_clicked();
+    void                on_website_clicked();
+
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow      *ui;
 };
 
 #endif // MAINWINDOW_H
